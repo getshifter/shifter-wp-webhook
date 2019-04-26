26f1463df08c4e2bfb4dd8b46df0c3002c460eb3
hideokamoto <hide.okamoto@digitalcube.jp>
2019-04-26 11:53:34 +0900

    init

---
+++ shifter-wp-webhook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Shifter Wp Webhook
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     shifter-wp-webhook
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Shifter_Wp_Webhook
+ */
+
+// Your code starts here.
