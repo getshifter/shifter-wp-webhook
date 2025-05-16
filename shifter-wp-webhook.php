8d39f13ee9a2a366e2bf25af031a0c4a6dfe85c9
sawanoboly <sawanoboriyu@higanworks.com>
2025-05-16 18:03:53 +0900

    bump 1.2.2

--- shifter-wp-webhook.php
+++ shifter-wp-webhook.php
@@ -7,7 +7,7 @@
  * Author URI:      https://getshifter.io
  * Text Domain:     shifter-wp-webhook
  * Domain Path:     /languages
- * Version:         1.2.1
+ * Version:         1.2.2
  *
  * @package         Shifter_Wp_Webhook
  */
